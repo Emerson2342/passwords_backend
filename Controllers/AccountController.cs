be3d9e98931a75bc68662f7649fb1d49215a4177
Emerson Ribeiro <lyncoln_erc@hotmail.com>
2025-11-05 12:06:22 -0300

    [REFACTOR]

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using passwords_backend.Data;
+using passwords_backend.Handlers;
 using passwords_backend.Models;
 
 namespace passwords_backend.Controllers
